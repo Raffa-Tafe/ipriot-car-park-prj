9ea091ef1dc3c416056cc6b98ae69ede71524728
Raffa Ramadhan <20126215@tafe.wa.edu.au>
2025-06-04 13:18:04 +0800

    Added a contructor and attributes to the park class

--- src/car_park.py
+++ src/car_park.py
@@ -1,2 +1,13 @@
 class CarPark:
-    pass
+    def __init__(self, location, capacity, plates=None, sensors=None, display=None):
+        self.location = location
+        self.capacity = capacity
+        self.plates = plates
+        self.sensors = sensors
+        self.display = display
+
+    def __str__(self):
+        return  #   When you print a Car park object, this method will be called. The method should return a string containing the car park's location and capacity. For example, "Car park at 123 Example Street, with 100 bays.".
+
+
+    
